216e74aa3b22bff685ddbdc0125e41155b91ed03
Ivy Chang <ivy900403@gmail.com>
2023-08-22 00:59:47 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -7,28 +7,24 @@ classifiers = [
     'Topic :: Scientific/Engineering :: Artificial Intelligence',
 ]
 
-requirements = [
-    'torch >= 1.12.1',
-    'numpy >= 1.19.2',
-    'scikit-learn >= 1.1.2',
-    'scipy >= 1.7.3',
-]
-
-
 setup (
     name='ParametricSpectralClustering',
-    version='0.0.13',
+    version='0.0.14',
     description='A library for users to use parametric spectral clustering',
     long_description=open("README.md").read() + '\n\n' + open("CHANGELOG.txt").read(),
     long_description_content_type = "text/markdown",
-    package_dir={"" : "ParametricSpectralClustering"},
-    packages=find_packages(where="ParametricSpectralClustering"),
+    packages=find_packages(),
     classifiers=classifiers,
     url='',
     author='Ivy Chang, Hsin Ju Tai',
     author_email='ivy900403@gmail.com, luludai020127@gmail.com',
     license='MIT',
-    install_requires=requirements,
+    install_requires=[
+        'torch >= 1.12.1',
+        'numpy >= 1.19.2',
+        'scikit-learn >= 1.1.2',
+        'scipy >= 1.7.3'
+    ],
     include_package_data=True,
     zip_safe=False,
     python_requires = ">=3.8",
