b3b8462c9dc91167ca64f559625acf7816de6529
Hsin Ju Tai <luludai020127@gmail.com>
2023-10-16 23:34:30 +0800

    Modify experiment (Fashion_MNIST)

--- experiment/Fashion_MNIST.py
+++ experiment/Fashion_MNIST.py
@@ -77,15 +77,21 @@ autoencoder = Autoencoder()
 autoencoder = torch.load("experiment\\autoencoder.pth")
 
 train_num = 60000
-cut_1 = 30000
+cut_1 = 15000
+cut_2 = 30000
+cut_3 = 45000
 
 X_train = train_data.data[:train_num]/255
 X_train = X_train.unsqueeze(1) 
 X_train = X_train.to(torch.float32)
 
-X_train_1 = autoencoder(X_train[:cut_1], stop=True).detach().numpy()
-X_train_2 = autoencoder(X_train[cut_1:], stop=True).detach().numpy()
-X_train = np.concatenate((X_train_1, X_train_2), axis=0)
+X_1 = autoencoder(X_train[:cut_1], stop=True).detach().numpy()
+X_2 = autoencoder(X_train[cut_1:cut_2], stop=True).detach().numpy()
+X_3 = autoencoder(X_train[cut_2:cut_3], stop=True).detach().numpy()
+X_4 = autoencoder(X_train[cut_3:train_num], stop=True).detach().numpy()
+X_train_1 = np.concatenate((X_1, X_2), axis=0)
+X_train_2 = np.concatenate((X_3, X_4), axis=0)
+X_train_full = np.concatenate((X_1, X_2, X_3, X_4), axis=0)
 
 y_train = train_data.targets[:train_num].numpy()
 print("finish data transformation")
@@ -94,10 +100,10 @@ kmeans = KMeans(n_clusters=10, init="k-means++", n_init=1, max_iter=100, algorit
 sc = SpectralClustering(n_clusters=10, assign_labels='discretize', random_state=0)
 model = Four_layer_FNN(49, 196, 392, 196, 10)
 
-# kmeans
+# kmeans, 60000 data
 print("\nkmeans")
 time1 = round(time.time()*1000)
-Kmeans_index = kmeans.fit_predict(X_train)
+Kmeans_index = kmeans.fit_predict(X_train_full)
 time2 = round(time.time()*1000)
 print(f"Kmeans time spent: {time2 - time1} milliseconds")
 
@@ -107,13 +113,12 @@ acc.acc_report()
 # spectral clustering: can't run with 60000 data
 # print("\nspectral clustering")
 # time1 = round(time.time()*1000)
-# SC_index = sc.fit_predict(X_train)
+# SC_index = sc.fit_predict(X_train_full)
 # time2 = round(time.time()*1000)
 # print(f"SC time spent: {time2 - time1} milliseconds")
 
 # acc = Accuracy(y_true=y_train, y_pred=SC_index)
 # acc.acc_report()
-
 
 # PSC + kmeans
 psc = PSC(model=model, clustering_method=kmeans, test_splitting_rate=0.7)
@@ -124,7 +129,7 @@ PSC_index_1 = psc.fit_predict(X_train_1)
 time2 = round(time.time()*1000)
 print(f"PSC time spent: {time2 - time1} milliseconds")
 
-acc = Accuracy(y_true=y_train[:cut_1], y_pred=PSC_index_1)
+acc = Accuracy(y_true=y_train[:cut_2], y_pred=PSC_index_1)
 acc.acc_report()
 
 print("\npredict() for 30000 data")
@@ -133,7 +138,7 @@ PSC_index_2 = psc.predict(X_train_2)
 time2 = round(time.time()*1000)
 print(f"PSC time spent: {time2 - time1} milliseconds")
 
-acc = Accuracy(y_true=y_train[cut_1:], y_pred=PSC_index_2)
+acc = Accuracy(y_true=y_train[cut_2:train_num], y_pred=PSC_index_2)
 acc.acc_report()
 
 print("\naccuracy rate for 60000 data")
@@ -141,71 +146,26 @@ result = np.concatenate((PSC_index_1, PSC_index_2), axis=0)
 acc = Accuracy(y_true=y_train, y_pred=result)
 acc.acc_report()
 
-cut_sc = 10000
-X_train_sc = autoencoder(X_train[:cut_sc], stop=True).detach().numpy()
-cut_sc1 = 5000 
-cut_sc2 = 10000
-cut_sc3 = 15000
-cut_sc4 = 20000
-X_train_sc1 = autoencoder(X_train[:cut_sc1], stop=True).detach().numpy()
-X_train_sc2 = autoencoder(X_train[cut_sc1:cut_sc2], stop=True).detach().numpy()
-X_train_sc3 = autoencoder(X_train[cut_sc2:cut_sc3], stop=True).detach().numpy()
-X_train_sc4 = autoencoder(X_train[cut_sc3:cut_sc4], stop=True).detach().numpy()
 
 # spectral clustering: can run with 10000 data(>3hrs)
 print("\nspectral clustering")
 print("\nfit_predict() for 10000 data")
 time1 = round(time.time()*1000)
-SC_index = sc.fit_predict(X_train_sc)
+SC_index = sc.fit_predict(X_train_full[:10000])
 time2 = round(time.time()*1000)
 print(f"SC time spent: {time2 - time1} milliseconds")
 
-acc = Accuracy(y_true=y_train[:cut_sc], y_pred=SC_index)
+acc = Accuracy(y_true=y_train[:10000], y_pred=SC_index)
 acc.acc_report()
 
-# PSC + sc
+# PSC + sc: spectral clustering don't have predict() method
 psc = PSC(model=model, clustering_method=sc, test_splitting_rate=0.7)
 print("\nPSC + sc")
-print("\nfit_predict() for 5000 data")
+print("\nfit_predict() for 10000 data")
 time1 = round(time.time()*1000)
-PSC_index_1 = psc.fit_predict(X_train_sc1)
+PSC_index_1 = psc.fit_predict(X_train_full[:10000])
 time2 = round(time.time()*1000)
 print(f"PSC time spent: {time2 - time1} milliseconds")
 
-acc = Accuracy(y_true=y_train[:cut_sc1], y_pred=PSC_index_1)
-acc.acc_report()
-
-print("5000~10000")
-print("\npredict() for 5000 data")
-time1 = round(time.time()*1000)
-PSC_index_2 = psc.predict(X_train_sc2)
-time2 = round(time.time()*1000)
-print(f"PSC time spent: {time2 - time1} milliseconds")
-
-acc = Accuracy(y_true=y_train[cut_sc1:cut_sc2], y_pred=PSC_index_2)
-acc.acc_report()
-
-print("10000~15000")
-print("\npredict() for 5000 data")
-time1 = round(time.time()*1000)
-PSC_index_3 = psc.predict(X_train_sc3)
-time2 = round(time.time()*1000)
-print(f"PSC time spent: {time2 - time1} milliseconds")
-
-acc = Accuracy(y_true=y_train[cut_sc2:cut_sc3], y_pred=PSC_index_3)
-acc.acc_report()
-
-print("15000~20000")
-print("\npredict() for 5000 data")
-time1 = round(time.time()*1000)
-PSC_index_4 = psc.predict(X_train_sc4)
-time2 = round(time.time()*1000)
-print(f"PSC time spent: {time2 - time1} milliseconds")
-
-acc = Accuracy(y_true=y_train[cut_sc3:cut_sc4], y_pred=PSC_index_4)
-acc.acc_report()
-
-print("\naccuracy rate for 20000 data")
-result = np.concatenate((PSC_index_1, PSC_index_2, PSC_index_3, PSC_index_4), axis=0)
-acc = Accuracy(y_true=y_train[:cut_sc4], y_pred=result)
+acc = Accuracy(y_true=y_train[:10000], y_pred=PSC_index_1)
 acc.acc_report()
