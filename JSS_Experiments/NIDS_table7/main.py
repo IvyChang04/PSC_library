b728cd7b87997ab6b218deb4e0a40a67857c59ad
Ivy Chang <ivy900403@gmail.com>
2024-02-04 15:03:05 +0800

    update file name and add NIDS/result.csv

--- JSS_Experiments/NIDS_table7/main.py
+++ JSS_Experiments/NIDS_table7/main.py
@@ -9,14 +9,14 @@ import argparse
 import warnings
 import numpy as np
 
-# python NIDS/exp.py --methods kmeans --size -1 --rate 0.1
+# python NIDS/exp.py --methods kmeans --size -1 --rate 0.9
 
 warnings.filterwarnings("ignore")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-datasize", "--size", type=int, help="data size used for training")
 parser.add_argument("-methods", "--methods", nargs="+", help="which method to test")
-parser.add_argument("-rate", "--rate", type=float, help="test splitting rate")
+parser.add_argument("-sampling_ratio", "--ratio", type=float, help="sampling ratio")
 args = parser.parse_args()
 
 
@@ -39,7 +39,7 @@ class Net_emb(nn.Module):
         return x
 
 
-df = pd.read_csv("NIDS/NF-UQ-NIDS-v2.csv", nrows=1040000)
+df = pd.read_csv("JSS_Experiments/datasets/NF-UQ-NIDS-v2.csv", nrows=1040000)
 Class = {
     "Benign": 1,
     "DDoS": 2,
@@ -69,7 +69,7 @@ print(df.drop(["Attack", "IPV4_SRC_ADDR", "IPV4_DST_ADDR", "Dataset"], axis=1).i
 y_tmp = df["Attack"].values
 x_tmp = df.drop(["Attack", "IPV4_SRC_ADDR", "IPV4_DST_ADDR", "Dataset"], axis=1).values
 
-f = open("JSS_Experiments/Large_dataset/log.txt", "a+")
+f = open("JSS_Experiments/NIDS_table7/log.txt", "a+")
 now = str(datetime.datetime.now())
 f.write("======" + now + "======\n")
 if args.size == -1:
@@ -90,6 +90,8 @@ total_time = []
 total_ari = []
 total_ami = []
 
+result = pd.read_csv("JSS_Experiments/NIDS_table7/result.csv", index_col=0)
+
 for i in range(10):
     # --------Spectral Clustering--------
     if "sc" in methods:
@@ -99,16 +101,21 @@ for i in range(10):
             affinity="nearest_neighbors",
             assign_labels="kmeans",
         )
+
+        # measure time spent
         start_time = round(time.time() * 1000)
         sc_index = spectral_clustering.fit_predict(x)
         end_time = round(time.time() * 1000)
-        print("time spent:", end_time - start_time)
+
+        # calculate acc, ari, ami
         acc = Accuracy(y_true=y, y_pred=sc_index)
         sc_accRate, sc_ari, sc_ami = acc.acc_report()
         total_acc.append(sc_accRate)
         total_ari.append(sc_ari)
         total_ami.append(sc_ami)
         total_time.append(end_time - start_time)
+
+        # write result into log.txt
         f.write("---SpectralClustering---\n")
         f.write("acc rate: " + str(sc_accRate) + "\n")
         f.write("ari: " + str(sc_ari) + "\n")
@@ -118,16 +125,21 @@ for i in range(10):
     # --------kmeans--------
     if "kmeans" in methods:
         kmeans = KMeans(n_clusters=10, init="random", n_init="auto", algorithm="elkan")
+
+        # measure time spent
         start_time = round(time.time() * 1000)
         kmeans_index = kmeans.fit_predict(x)
         end_time = round(time.time() * 1000)
-        print("time spent:", end_time - start_time)
+
+        # calculate acc, ari, ami
         acc = Accuracy(y_true=y, y_pred=kmeans_index)
         kmeans_accRate, kmeans_ari, kmeans_ami = acc.acc_report()
         total_acc.append(kmeans_accRate)
         total_ari.append(kmeans_ari)
         total_ami.append(kmeans_ami)
         total_time.append(end_time - start_time)
+
+        # write result into log.txt
         f.write("---------Kmeans---------\n")
         f.write("acc rate: " + str(kmeans_accRate) + "\n")
         f.write("ari: " + str(kmeans_ari) + "\n")
@@ -146,29 +158,41 @@ for i in range(10):
             n_neighbor=10,
             batch_size_data=args.size,
         )
+
+        # measure time spent
         start_time = round(time.time() * 1000)
         psc.fit(x)
         psc_index = psc.predict(x)
         end_time = round(time.time() * 1000)
-        print("time spent:", end_time - start_time)
+
+        # save model
         psc.save_model("NIDS/psc_model" + str(i + 1) + ".pkl")
+
+        # calculate acc, ari, ami
         acc = Accuracy(y_true=y, y_pred=psc_index)
         psc_accRate, psc_ari, psc_ami = acc.acc_report()
         total_acc.append(psc_accRate)
         total_ari.append(psc_ari)
         total_ami.append(psc_ami)
         total_time.append(end_time - start_time)
+
+        # write result into log.txt
         f.write("----------PSC----------\n")
         f.write("acc rate: " + str(psc_accRate) + "\n")
         f.write("ari: " + str(psc_ari) + "\n")
         f.write("ami: " + str(psc_ami) + "\n")
         f.write("time spent: " + str(end_time - start_time) + "\n\n\n")
 
+# compute time, acc, ari, ami mean±std
 ari_mean, ari_std = np.mean(total_ari), np.std(total_ari)
 ami_mean, ami_std = np.mean(total_ami), np.std(total_ami)
 acc_mean, acc_std = np.mean(total_acc), np.std(total_acc)
 time_mean, time_std = np.mean(total_time), np.std(total_time)
+
+# write mean±std into log.txt
 f.write("==============report==============\n")
+f.write("|data size: " + str(args.size) + "|\n")
+f.write("|method: " + str(args.methods) + "|\n")
 f.write("|acc: " + str(acc_mean) + "±" + str(acc_std) + "|\n")
 f.write("|ari: " + str(ari_mean) + "±" + str(ari_std) + "|\n")
 f.write("|ami: " + str(ami_mean) + "±" + str(ami_std) + "|\n")
@@ -182,4 +206,19 @@ print("ami:", ami_mean, "±", ami_std)
 print("time:", time_mean, "±", time_std)
 print("===========================\n\n\n")
 
+
+# write result into result.csv
+if "psc" in methods:
+    result.at[str(args), "PSC"] = str(time_mean) + "±" + str(time_std)
+    result.at[str(args), "PSC.1"] = str(acc_mean) + "±" + str(acc_std)
+    result.at[str(args), "PSC.2"] = str(ari_mean) + "±" + str(ari_std)
+    result.at[str(args), "PSC.3"] = str(ami_mean) + "±" + str(ami_std)
+
+if "sc" in methods:
+    result.at[str(args), "SC"] = str(time_mean) + "±" + str(time_std)
+    result.at[str(args), "SC.1"] = str(acc_mean) + "±" + str(acc_std)
+    result.at[str(args), "SC.2"] = str(ari_mean) + "±" + str(ari_std)
+    result.at[str(args), "SC.3"] = str(ami_mean) + "±" + str(ami_std)
+
 f.close()
+df.to_csv("JSS_Experiments/NIDS_table7/result.csv")
--- JSS_Experiments/run.sh
+++ JSS_Experiments/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # set -ex
 
+# experiment for table 3
 for data in 15000, 30000, 45000, 60000
 do
     python JSS_Experiments/Firewall_table3/fixed_n_wo_sampling_ratio.py --methods sc --size ${data}
@@ -8,7 +9,16 @@ done
 
 python JSS_Experiments/Firewall_table3/fixed_n_wo_sampling_ratio.py --methods psc --size 60000
 
+# experiment for table 4
 for data in 15000, 30000, 45000, 60000
 do
     python JSS_Experiments/Firewall_table4/fixed_n_with_sampling_ratio.py --methods psc sc --size ${data}
 done
+
+# experiment for table 7
+for data in 50000, 200000, 1040000
+do
+    python JSS_Experiments/Large_dataset/main.py --methods psc --size ${size} --rate 0.9
+    python JSS_Experiments/Large_dataset/main.py --methods sc --size ${size}
+done
+
