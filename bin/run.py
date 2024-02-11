b1f518bac9d42fb2f0aeedf43da32b368c8f9fb5
Hsin Ju Tai <luludai020127@gmail.com>
2024-02-11 11:29:49 +0200

    update cmd tool

--- bin/run.py
+++ bin/run.py
@@ -9,10 +9,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-data", "--train_data", type=str, help="Training data")
 parser.add_argument(
     "-rate",
-    "--test_splitting_rate",
+    "--sampling_ratio",
     type=float,
     default=0.3,
-    help="The splitting rate of the testing data",
+    help="The spliting rate of the testing data.",
 )
 parser.add_argument(
     "-n_cluster", "--n_cluster", type=int, default=10, help="Number of clusters"
@@ -31,6 +31,13 @@ parser.add_argument(
     default="csv",
     help="Save the result in csv format or txt format",
 )
+parser.add_argument(
+    "-load_model_path",
+    "--load_model_path",
+    type=str,
+    default=None,
+    help="The path that loads the trained model",
+    )
 args = parser.parse_args()
 
 
@@ -41,11 +48,11 @@ def __check_args():
         "txt",
     ]:
         raise ValueError("The training data must be in .npy, .csv or .txt format.")
-    if args.test_splitting_rate is not None and (
-        args.test_splitting_rate <= 0 or args.test_splitting_rate > 1
+    if args.sampling_ratio is not None and (
+        args.sampling_ratio <= 0 or args.sampling_ratio > 1
     ):
         raise ValueError(
-            "Test_splitting_rate must be floating point and between 0 and 1."
+            "The sampling_ratio must be floating point and between 0 and 1."
         )
     if args.n_cluster is not None and args.n_cluster <= 0:
         raise ValueError("n_cluster must be integer and greater than 0.")
@@ -62,7 +69,7 @@ def __load_data():
     elif args.train_data[-3:] == "csv":
         df = pd.read_csv(args.train_data, header=None)
         x = df.to_numpy()
-    elif args.train_data[:-3] == "txt":
+    elif args.train_data[-3:] == "txt":
         x = np.loadtxt(args.train_data, dtype=int)
 
     return x
@@ -73,19 +80,24 @@ def main(argv):
 
     x = __load_data()
 
-    cluster_method = KMeans(
-        n_clusters=args.n_cluster,
-        init="k-means++",
-        n_init=1,
-        max_iter=100,
-        algorithm="elkan",
-    )
-    psc = PSC(
-        clustering_method=cluster_method,
-        test_splitting_rate=args.test_splitting_rate,
-        n_neighbor=args.n_cluster,
-    )
-    cluster_idx = psc.fit_predict(x)
+    if args.load_model_path is not None:
+        psc = PSC()
+        psc.load_model(args.load_model_path)
+        cluster_idx = psc.predict(x)
+    else:
+        cluster_method = KMeans(
+            n_clusters=args.n_cluster,
+            init="k-means++",
+            n_init=1,
+            max_iter=100,
+            algorithm="elkan",
+        )
+        psc = PSC(
+            clustering_method=cluster_method,
+            sampling_ratio=args.sampling_ratio,
+            n_neighbor=args.n_cluster,
+        )
+        cluster_idx = psc.fit_predict(x)
 
     if args.model_path is not None:
         psc.save_model(args.model_path)
